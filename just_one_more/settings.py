4d464d57dfdbf259244f3f4f318ffcbf66205e88
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-08 22:37:51 +0000

    installed crispy forms

--- just_one_more/settings.py
+++ just_one_more/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'just_one_more.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -97,6 +100,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',  # Requried to use {{ MEDIA_URL }}
                 'bag.contexts.bag_contents'
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
