76b4b1e1b2b1a6891abf028ef0f9ee76729b0767
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-08 19:21:35 +0000

    added add to bag function and added some more products

--- bag/views.py
+++ bag/views.py
@@ -18,3 +18,32 @@ def add_to_basket(request, item_id):
     """
 
     product = get_object_or_404(Product, pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
+    size = None
+    quantity = int(request.POST.get('quantity'))
+
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'Updated Size {size.upper()} {product.name} quantity to {bag[bag_id]["items_by_size"][size]}!')
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()} {product.name} to your bag!')
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added Size {size.upper()} {product.name} to your bag!')
+    else:
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}!')
+        else:
+            bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag!')
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
