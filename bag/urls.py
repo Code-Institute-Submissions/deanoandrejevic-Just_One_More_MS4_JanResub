6f447dd0c29477031f8b81af213e8c614ffc8769
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-08 19:46:33 +0000

    added more products and linked up add to basket button

--- bag/urls.py
+++ bag/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>', views.add_to_basket, name='add_to_basket')
 ] 
