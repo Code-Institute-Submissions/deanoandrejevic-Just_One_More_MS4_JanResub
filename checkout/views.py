c96b6823e712db7a9bf3ed75d52842166fb3b2f9
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-09 13:17:44 +0000

    added urls and basic view to render checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def checkout(request):
+    """
+    This will display the bag page.
+    """
+
+    return render(request, 'checkout/checkout.html')
