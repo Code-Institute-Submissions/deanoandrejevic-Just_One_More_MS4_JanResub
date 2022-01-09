eb5e5bea2ec2c4bbc196e38be3b81a04505c799c
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-09 15:32:18 +0000

    added js file for stripe payments and copied stripe css elements from stripe docs

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,7 @@
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = stripe.elements();
+var elements = stripe.elements();
+var style = {
+    
+}
--- checkout/views.py
+++ checkout/views.py
@@ -20,6 +20,8 @@ def checkout(request):
 
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51K08mLBRYqT3cZ1NVVD3DTMjakcor32LEbhnP4Xd1auWF4a4YQnnUwaB6tR3abocI1pkZ98MyDmxf1VRV1GNhqpq00QS5RHGWH',
+        'client_secret': 'client secret'
     }
 
     return render(request, template, context)
