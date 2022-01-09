e1cd86c1e604204a68b34c8d50f3da7bef9d3c9b
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-09 16:46:59 +0000

    added stripe elements to checkout page and starting linking to stripe

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,7 +1,24 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = stripe.elements();
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
-    
-}
+    base: {
+        color: '#',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+
