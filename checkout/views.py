88588964aaeb4e9b02d3ead2167b6c9e6d4a2874
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-09 21:15:59 +0000

    added form caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -7,10 +8,27 @@ from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
 
+import json
 import stripe
 
 # Create your views here.
 
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        payment_id = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(payment_id, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, "Sorry your payment has not been processed")
+        return HttpResponse(content=e, status=400)
+    
 
 def checkout(request):
     """
