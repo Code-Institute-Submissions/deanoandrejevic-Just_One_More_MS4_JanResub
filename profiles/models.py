ca26b6007cf2928f9b65ee1f1a7368c7ea4d203d
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-09 23:47:45 +0000

    user can now log in, having issue with registering users

--- profiles/models.py
+++ profiles/models.py
@@ -7,14 +7,18 @@ from django_countries.fields import CountryField
 
 # Create your models here.
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_mob_number = models.CharField(max_length=15, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=15, null=True, blank=True)
     default_city = models.CharField(max_length=30, null=True, blank=True)
-    default_first_address_line = models.CharField(max_length=80, null=True, blank=True)
-    default_second_address_line = models.CharField(max_length=80, null=True, blank=True)
+    default_first_address_line = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_second_address_line = models.CharField(
+        max_length=80, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
@@ -26,7 +30,7 @@ def create_or_update_user(sender, instance, created, **kwargs):
     """
     Create or update userprofiles
     """
-    if created: 
+    if created:
         UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile    
+    # Existing users: just save the profile
     instance.userprofile.save()
