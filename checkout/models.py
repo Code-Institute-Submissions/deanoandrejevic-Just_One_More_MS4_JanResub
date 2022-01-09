c7a600b7b1f648046fd265e8865cbaed6fac2eed
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-09 10:27:36 +0000

    added more to models to be migrated

--- checkout/models.py
+++ checkout/models.py
@@ -4,26 +4,51 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
-from django.models import Product
+from products.models import Product
 
 # Create your models here.
 
 class Order(models.Model):
-    order_number = models.Charfield(max_length=48, null=false)
-    full_name = models.Charfield(max_length=48, null=false, blank=false)
-    email = models.EmailField(max_length=256, null=false, blank=false)
-    mob_number = models.Charfield(max_length=15, null=false, blank=false)
-    country = models.Charfield(max_length=20, null=false, blank=false)
-    postcode = models.Charfield(max=length=15, null=True, blank=True)
-    city = models.Charfield(max_length=30, null=false, blank=false)
-    first_address_line = models.Charfield(max_length=80, null=false, blank=false)
-    second_address_line = models.Charfield(max_length=80, null=false, blank=false)
-    county = models.Charfield(max_length=80, null=True, blank=True)
+    """
+    This class will be migrated into the admin, this will generate
+    an order report which will be viewable to the user once they
+    have created a user account
+    """
+    order_number = models.CharField(max_length=32, null=False)
+    full_name = models.CharField(max_length=48, null=False, blank=False)
+    email = models.EmailField(max_length=256, null=False, blank=False)
+    mob_number = models.CharField(max_length=15, null=False, blank=False)
+    country = models.CharField(max_length=20, null=False, blank=False)
+    postcode = models.CharField(max_length=15, null=True, blank=True)
+    city = models.CharField(max_length=30, null=False, blank=False)
+    first_address_line = models.CharField(max_length=80, null=False, blank=False)
+    second_address_line = models.CharField(max_length=80, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    delivery = models.DecimalField(max_digits=5, decimal_places=2, null=false, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=false, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=false, default=0)
+    delivery = models.DecimalField(max_digits=5, decimal_places=2, null=False, default=4.99)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """
+        Generates a random and unique order number
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        updates order total viewable in grand_total
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.grand_total = self.order_total + self.delivery
+        self.save()
 
 
 class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=3, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
+    
