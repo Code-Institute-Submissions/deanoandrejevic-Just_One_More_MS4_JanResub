ee88ea639be301b74c797d75ccb6c4ddf7ad6fad
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-10 19:21:51 +0000

    added views for CRUD function with product management

--- products/views.py
+++ products/views.py
@@ -85,24 +85,69 @@ def product_detail(request, product_id):
 
 @login_required
 def add_product(request):
-
+    """ Add a product to the shop """
     if not request.user.is_superuser:
+        messages.error(request, 'Sorry you need to be a super user to do that')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
-            messages.success(request, 'Product added')
+            messages.success(request, 'Product Added')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Update failed')
+            messages.error(
+                request, 'Failed, Make sure everything is valid and try again')
     else:
         form = ProductForm()
 
     template = 'products/add_product.html'
     context = {
-        'form': form
+        'form': form,
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """ Edit a product in the shop """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry you need to be a super user to do that')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product Updated')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request, 'Failed, Make sure everything is valid and try again')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product from the shop """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry you need to be a super user to do that')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
