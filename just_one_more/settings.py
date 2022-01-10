d8429b7c7f7a6d6017b11a3d126a7e26a7f9362e
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-10 15:45:25 +0000

    json dumped data from dB to load into postgres db

--- just_one_more/settings.py
+++ just_one_more/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY','.')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -185,7 +185,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max=age=94608000'
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'just-one-more-ms4'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -202,6 +202,8 @@ if 'USE_AWS' in os.environ:
     #Override static and media URLS in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
