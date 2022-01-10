a265905387158e1e6a980d32423c23ad28eb656d
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-10 03:03:42 +0000

    added send email functionality

--- just_one_more/settings.py
+++ just_one_more/settings.py
@@ -182,3 +182,4 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_k0psG9SrYmaqBpzt9WJNFptSC24SrCIV')
+DEFAULT_FROM_EMAIL = 'justonemore@example.com'
