53e3c932e30c3e47d9d346f2c663140a4ef2d5f6
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-10 00:44:36 +0000

    render form in profile template

--- profiles/models.py
+++ profiles/models.py
@@ -11,15 +11,15 @@ from django_countries.fields import CountryField
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_mob_number = models.CharField(max_length=15, null=True, blank=True)
-    default_country = CountryField(
-        blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=15, null=True, blank=True)
-    default_city = models.CharField(max_length=30, null=True, blank=True)
     default_first_address_line = models.CharField(
         max_length=80, null=True, blank=True)
     default_second_address_line = models.CharField(
         max_length=80, null=True, blank=True)
+    default_city = models.CharField(max_length=30, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=15, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country *', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 
 def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profiles.html'
     context = {
+        'form': form,
         'profile': profile,
+        'orders': orders,
     }
 
     return render(request, template, context)
