fd719db23e6a62dfdcff8cb1a23ce642eea4f1ba
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-11 00:33:15 +0000

    added review form

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category
+from .models import Product, Category, Review, ONE_TO_FIVE
 
 class ProductForm(forms.ModelForm):
 
@@ -15,3 +15,20 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = "border-black rounded-0"
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'user_review': 'Review',
+            'rate' : 'Rating'
+        }
+
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].label = False
